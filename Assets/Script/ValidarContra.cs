a7f951a8764e3b485c69e756bec1f782b9c415e5
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2022-04-22 15:35:08 -0300

    :+1:

---
+++ Assets/Script/ValidarContra.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContra : MonoBehaviour
+{
+
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text IngresoUsuario;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ValidarContrasenia()
+    {
+        contraseniaUsuario = IngresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
--- Assets/Script/hola.cs
+++ Assets/Script/hola.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hola : MonoBehaviour
 {
+    public int edad;
+    public Text miTexto;
     // Start is called before the first frame update
     void Start()
     {
         
+        if (edad >= 18)
+        {
+            miTexto.text = "Es mayor de edad";
+        }
+
+        else
+        {
+            miTexto.text = "Es menor de edad";
+        }
     }
 
     // Update is called once per frame
